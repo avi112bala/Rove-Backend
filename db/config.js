9d18f046a34d5276c776efb4c7a46aa241614b9f
avi112bala <avinash20802bala@gmail.com>
2024-09-02 05:40:26 +0530

    Update

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectWithRetry = () => {
   return mongoose.connect(
-      "mongodb+srv://avi20802bala:Tech1216@cluster0.xsxuhan.mongodb.net/RoveIndia?retryWrites=true&w=majority&appName=Cluster0",
+      "mongodb+srv://avi116:Tech1216@cluster0.xsxuhan.mongodb.net/RoveIndia?retryWrites=true&w=majority&appName=Cluster0",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
